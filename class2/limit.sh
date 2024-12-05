6c7f2c5dfcf6892685363839f9f4e604c9da1be8
EC2 Default User <ec2-user@ip-172-31-94-125.ec2.internal>
2024-12-05 02:53:47 +0000

    class-2 files

---
+++ class2/limit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -n 4 -p "Enter year: " year
+
+echo
+echo you were born in $year
---
+++ class2/tip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter your tip: " tip
+
+if [ $tip -eq 15 ]
+then
+	echo "Standard"
+elif [ $tip -eq 18 ]
+then 
+	echo "Good"
+elif [ $tip -eq 20 ]
+then 
+        echo "Great"
+elif [ $tip -gt 20 ]
+then
+        echo "my hero"
+else
+	echo "Wrong number"
+fi	
